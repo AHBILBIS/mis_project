2121ed22c485ad7501d06365da615782a25d9f02
AHBILBIS <aminuhalilu4@gmail.com>
2026-08-08 18:23:56 +0800

    Set Login as default landing route and add register option link

--- core_mis/urls.py
+++ core_mis/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
+    path("", lambda request: redirect("login"), name="root"),
     path("admin/", admin.site.urls),
     path("accounts/", include("apps.accounts.urls")),
     path("staff/", include("apps.staff.urls")),
