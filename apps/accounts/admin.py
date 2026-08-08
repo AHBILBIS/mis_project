159d7015c95db160fac13aafe4f42eb6d9514f38
AHBILBIS <aminuhalilu4@gmail.com>
2026-08-08 13:05:28 +0800

    Explicitly define fieldsets and user forms for CustomUserAdmin

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,23 +1,42 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ("username", "email")
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
+        fields = ("username", "email")
+
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
     model = CustomUser
+
     list_display = ['username', 'email', 'employee_id', 'role', 'is_staff', 'is_active']
-    
-    # Custom fields displayed when editing an existing user
-    fieldsets = UserAdmin.fieldsets + (
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
         ('Enterprise Information', {
             'fields': ('role', 'employee_id', 'phone_number', 'profile_picture', 'is_verified')
         }),
     )
-    
-    # Custom fields displayed when adding a new user
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Enterprise Information', {
+
+    add_fieldsets = (
+        (None, {
             'classes': ('wide',),
-            'fields': ('role', 'employee_id', 'phone_number', 'is_verified'),
+            'fields': ('username', 'email', 'role', 'employee_id', 'phone_number', 'is_verified'),
         }),
     )
