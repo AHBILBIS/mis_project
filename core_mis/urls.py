392b60b5abbf864453a30604ca9320b5bbe78cbd
AHBILBIS <aminuhalilu4@gmail.com>
2026-08-09 12:37:15 +0800

    Add root URL redirect to storefront and disable DEBUG mode

--- core_mis/urls.py
+++ core_mis/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/staff/store/', permanent=False)),
     path('admin/', admin.site.urls),
     path('accounts/', include('apps.accounts.urls')),
     path('staff/', include('apps.staff.urls')),
-    path('', include('apps.accounts.urls')),
 ]
+
