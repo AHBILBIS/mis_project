8bf702cb72c1b0b132a3a7bb2cbe70cde967c56d
AHBILBIS <aminuhalilu4@gmail.com>
2026-08-09 10:10:25 +0800

    Restore staff_list URL route to resolve dashboard template rendering error

--- apps/staff/urls.py
+++ apps/staff/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.staff_list, name="staff_list"),
+    path("add/", views.staff_create, name="staff_create"),
     path("inventory/", views.inventory_list, name="inventory_list"),
     path("inventory/add/", views.inventory_create, name="inventory_create"),
     path("inventory/export/", views.export_inventory_excel, name="export_inventory_excel"),
